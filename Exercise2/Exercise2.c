673a3108e86d3d65c4b98a36491a82994bffbde8
Kaipapar <kkorsu@gmail.com>
2023-01-25 21:01:43 +0200

    more Ex2

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -7,14 +7,18 @@ Description:	read 2 int from user one by one.
 #include <stdio.h>
 
 int main() {
-	int num1;
-	int num2;
+	char num1;
+	char num2;
 	//char input;
-	printf("Input an integer:");
-	scanf("%d", &num1);
-	printf("You input: %d", num1);
+	printf("Input an integer: ");
+	scanf("%c", &num1);
+	printf("===\nInput another integer: ");
+	scanf(" %c", &num2);
+	printf("%d",num1);
 	
-
+	int num3 = num1 - '0';
+	
+	printf("\n%d",num3);
 	return 0;
 }
 
--- Exercise2/scanf_ex.c
+++ Exercise2/scanf_ex.c
@@ -4,54 +4,64 @@ Author:			Karri Korsu
 Description:	read 2 int from user one by one and print them.
 */
 
+#include <math.h>
 #include <stdio.h>
+
 
 int main() {
 
-	char num1;
-	char num2;
+	char num1Char;
+	char num2Char;
 
 	printf("Input an integer: ");
-	scanf("%c", &num1);
+	scanf("%c", &num1Char);
 	printf("===\nInput another integer: ");
-	scanf(" %c", &num2);
+	scanf(" %c", &num2Char);
+
 	
-	if (48 > num1 || num1 > 57) 
+	if (48 > num1Char || num1Char > 57) 
 	{
-		printf("===\nPlease input 2 integers.\n");
+		printf("===\n1Please input 2 integers.\n");
 	
-	} else if (48 > num2 || num2 > 57) 
+	} else if (48 > num2Char || num2Char > 57) 
 	{
-		printf("===\nPlease input 2 integers.\n");
+		printf("===\n2Please input 2 integers.\n");
 	
 	} else 
 	{
-		printf("===\nThe two integers you inputted are: %c and %c\n", num1, num2);
+		printf("===\nThe two integers you inputted are: %c and %c\n", num1Char, num2Char);
 		
-		if (num1 == num2) 
+		if (num1Char == num2Char) 
 		{
 			printf("===\nAccording to our calculations the two integers you inputted are equal!\n");
 			
-		} else if (num1 > num2) 
+		} else if (num1Char > num2Char) 
 		{
-			printf("===\nAccording to our calculations the first integer you inputted (%c) is bigger than the second (%c)\n", num1, num2);
+			printf("===\nAccording to our calculations the first integer you inputted (%c) is bigger than the second (%c)\n", num1Char, num2Char);
 	
 		} else 
 		{
-			printf("===\nAccording to our calculations the second integer you inputted (%c) is bigger than the first (%c)\n", num2, num1);
+			printf("===\nAccording to our calculations the second integer you inputted (%c) is bigger than the first (%c)\n", num2Char, num1Char);
 		}
 		
-		int sumOfInt;
-		int differenceOf1st2nd;
-		int differenceOf2nd1st;
-		int productOfInt;
-		int divisionOf1st2nd;
-		int divisionOf2nd1st;
+
+		int num1 = num1Char - '0';
+		int num2 = num2Char - '0';
 		
-		sumOfInt = num1 + num2;
-		differenceOf1st2nd = num1 - num2;
-		differenceOf2nd1st = num2 - num1;
-		productOfInt = num1 * num2;
+		int sumOfInt = num1 + num2;
+		int differenceOf1st2nd = num1 - num2;
+		int differenceOf2nd1st = num2 - num1;
+		int productOfInt = num1 * num2;
+		float averageOfInt = (num1*1.0 + num2*1.0) / 2;
+		int powerOfInt = round(pow(num1, num2));
+		//int powerOfInt = (int)(pow(num1, num2) + 1e-9);
+		printf("===\nSum of two integers: %d\n",sumOfInt);
+		printf("===\nDifference of the first and second integer: %d\n", differenceOf1st2nd);
+		printf("===\nDifference of the second and first integer: %d\n", differenceOf2nd1st);
+		printf("===\nProduct of two integers: %d\n", productOfInt);
+		printf("===\nAverage of two integers: %.2f\n", averageOfInt);
+		printf("===\nFirst integer raised to the power of the second: %d\n", powerOfInt);
+		
 		if (num1 == 0 || num2 == 0) 
 		{
 			if (num1 == num2) 
@@ -60,14 +70,20 @@ int main() {
 
 			} else if (num1 == 0) 
 			{
-				divisionOf
-				printf("===\n
+				printf("===\nThe first integer is 0, therefore in divisions between the two integers \nthe first one cannot be the denominator and when it is the numerator \nthe division equals to 0.\n");
+			} else if (num2 == 0) 
+			{
+				printf("===\nThe second integer is 0, therefore in divisions between the two integers \nthe second one cannot be the denominator and when it is the numerator \nthe division equals to 0.\n");			
 			}
 		}else 
 		{
-			divisionOf1st2nd = num1 / num2;
-			divisionOf2nd1st = num2 / num1;
+			float divisionOf1st2nd = (num1*1.0) / (num2*1.0);
+			float divisionOf2nd1st = (num2*1.0) / (num1*1.0);
+			
+			printf("===\nDivision between the first and second integer: %.2f\n", divisionOf1st2nd);
+			printf("===\nDivision between the second and first integer: %.2f\n", divisionOf2nd1st);
 		}
+		
 		
 	}
 	
