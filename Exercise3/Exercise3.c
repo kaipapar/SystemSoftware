ae0d1d4896603f18249602e034048a4435cfd00a
Kaipapar <kkorsu@gmail.com>
2023-01-31 14:26:21 +0200

    Added Ex3

---
+++ Exercise3/Exercise3.c
@@ -0,0 +1,26 @@
+/*
+File:			Exercise3.c
+Author:			Karri Korsu
+Description:	Answers to ex3
+*/
+
+#include <stdio.h>
+
+void sumOfNums();
+
+int main() 
+{
+	int num1
+
+	
+	return 0;
+}
+
+
+void sumOfNums() 
+{
+	
+
+
+
+}
