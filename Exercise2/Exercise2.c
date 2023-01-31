085a67df08963cab76df7448ce58c0e856255da3
Kaipapar <kkorsu@gmail.com>
2023-01-31 12:11:25 +0200

    remove unnecessary file

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -13,55 +13,55 @@ int main() {
 /* Task 4 */
 	
 	//initializing number variables as type char for error checking
-	char num1Char;
-	char num2Char;
+	char num1Str[10];
+	char num2Str[10];
 
-	printf("Input an integer between 0 and 9: ");
-	scanf("%c", &num1Char);
-	printf("===\nInput another integer between 0 and 9: ");
-	scanf(" %c", &num2Char);
+	printf("Input an integer: ");
+	scanf("%s", &num1Str);
+	printf("===\nInput another integer: ");
+	scanf(" %s", &num2Str);
+	
+	// atoi() function converts str into int
+	int num1Int = atoi(num1Str);
+	int num2Int = atoi(num2Str);
 
 	// checking whether user inputted characters can be interpreted
 	// as integers using the ascii values for them
-	if (48 > num1Char || num1Char > 57) 
+	if (48 > num1Str[0] || num1Str[0] > 57) 
 	{
 		printf("===\nPlease input 2 integers.\n");
 	
-	} else if (48 > num2Char || num2Char > 57) 
+	} else if (48 > num2Str[0] || num2Str[0] > 57) 
 	{
 		printf("===\nPlease input 2 integers.\n");
 	
 	} else 
 	{	
-		printf("===\nThe two integers you inputted are: %c and %c\n", num1Char, num2Char);
+		printf("===\nThe two integers you inputted are: %d and %d\n", num1Int, num2Int);
 		
-		if (num1Char == num2Char) 
+		if (num1Int == num2Int) 
 		{
 			printf("===\nAccording to our calculations the two integers you inputted are equal!\n");
 			
-		} else if (num1Char > num2Char) 
+		} else if (num1Int > num2Int) 
 		{
-			printf("===\nAccording to our calculations the first integer you inputted (%c)\
-			\nis bigger than the second (%c)\n", num1Char, num2Char);
+			printf("===\nAccording to our calculations the first integer you inputted (%s)\
+			\nis bigger than the second (%s)\n", num1Str, num2Str);
 	
 		} else 
 		{
-			printf("===\nAccording to our calculations the second integer you inputted (%c)\
-			 \nis bigger than the first (%c)\n", num2Char, num1Char);
+			printf("===\nAccording to our calculations the second integer you inputted (%d)\
+			 \nis bigger than the first (%d)\n", num2Int, num1Int);
 		}
-		
-		// converting inputted char numbers to type int for calculations
-		int num1 = num1Char - '0';
-		int num2 = num2Char - '0';
-		
-		int sumOfInt = num1 + num2;
-		int differenceOf1st2nd = num1 - num2;
-		int differenceOf2nd1st = num2 - num1;
-		int productOfInt = num1 * num2;
+				
+		int sumOfInt = num1Int + num2Int;
+		int differenceOf1st2nd = num1Int - num2Int;
+		int differenceOf2nd1st = num2Int - num1Int;
+		int productOfInt = num1Int * num2Int;
 		
 		// converting integer to float to get decimal values.
-		float averageOfInt = (num1*1.0 + num2*1.0) / 2;
-		int powerOfInt = pow(num1, num2);
+		float averageOfInt = (num1Int*1.0 + num2Int*1.0) / 2;
+		int powerOfInt = pow(num1Int, num2Int);
 
 		printf("===\nSum of two integers: %d\n",sumOfInt);
 		printf("===\nDifference of the first and second integer: %d\n", differenceOf1st2nd);
@@ -71,18 +71,18 @@ int main() {
 		printf("===\nFirst integer raised to the power of the second: %d\n", powerOfInt);
 		
 		//checking whether division of the integers is possible by checking is either of the integers 0
-		if (num1 == 0 || num2 == 0) 
+		if (num1Int == 0 || num2Int == 0) 
 		{
-			if (num1 == num2) 
+			if (num1Int == num2Int) 
 			{
 				printf("===\nBoth numbers are 0, they can't be divided.\n");
 
-			} else if (num1 == 0) 
+			} else if (num1Int == 0) 
 			{
 				printf("===\nThe first integer is 0, therefore in divisions between the two integers  \
 				\nthe first one cannot be the denominator and when it is the numerator \
 				\nthe division equals to 0.\n");
-			} else if (num2 == 0) 
+			} else if (num2Int == 0) 
 			{
 				printf("===\nThe second integer is 0, therefore in divisions between the two integers \
 				 \nthe second one cannot be the denominator and when it is the numerator\
@@ -90,8 +90,8 @@ int main() {
 			}
 		}else 
 		{
-			float divisionOf1st2nd = (num1*1.0) / (num2*1.0);
-			float divisionOf2nd1st = (num2*1.0) / (num1*1.0);
+			float divisionOf1st2nd = (num1Int*1.0) / (num2Int*1.0);
+			float divisionOf2nd1st = (num2Int*1.0) / (num1Int*1.0);
 			
 			printf("===\nDivision between the first and second integer: %.2f\n", divisionOf1st2nd);
 			printf("===\nDivision between the second and first integer: %.2f\n", divisionOf2nd1st);
@@ -104,18 +104,18 @@ int main() {
 	time_t t;
 	srand((unsigned) time(&t));
 	
-	int rand_num;
+	int randNum;
 	
-	// Assigns random integer to rand_num
+	// Assigns random integer to randNum
 	printf("===\nRandom number between 0 and 100: %d\n" \
-			, rand_num = rand() % 100);
+			, randNum = rand() % 100);
 	
 	
-	if (rand_num == 0) 
+	if (randNum == 0) 
 	{
 		printf("===\nThe random number is neither odd nor even; it is zero!\n");
 	
-	} else if (rand_num % 2 == 0) 
+	} else if (randNum % 2 == 0) 
 	{
 		printf("===\nThe random number is even.\n");
 	
@@ -125,7 +125,7 @@ int main() {
 		
 	}
 	
-	if (rand_num % 4 == 0 && rand_num % 7 == 0) 
+	if (randNum % 4 == 0 && randNum % 7 == 0) 
 	{
 		printf("===\nThe random number is divisible by 4 and 7\n");
 	
@@ -137,21 +137,21 @@ int main() {
 
 /* Task 6 */
 	
-	int int_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int intArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	int i;
 	int n = 0;
-	int num_check = 0;
+	int numCheck = 0;
 		
-	// Two for loops to print int_array in correct and reverse order
+	// Two for loops to print intArray in correct and reverse order
 	printf("===\n");
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d\n", int_array[i]);
+		printf("%d\n", intArray[i]);
 		n++;
 		
-		if (rand_num == int_array[i]) 
+		if (randNum == intArray[i]) 
 		{
-			num_check = 1;
+			numCheck = 1;
 			
 		}
 		
@@ -164,17 +164,17 @@ int main() {
 	
 	for (i = 9; i >= 0; i--)
 	{
-		printf("%d\n", int_array[i]);
+		printf("%d\n", intArray[i]);
 		n++;
 		
 	}
-	if (num_check) 
+	if (numCheck) 
 	{
-		printf("===\nThe random number (%d) is in the array!\n", rand_num);
+		printf("===\nThe random number (%d) is in the array!\n", randNum);
 		
 	} else
 	{
-		printf("===\nThe random number (%d) is not in the array!\n", rand_num);
+		printf("===\nThe random number (%d) is not in the array!\n", randNum);
 	}
 	
 	
