d312e1581d39b228ab27199f13584ab11119be23
Kaipapar <kkorsu@gmail.com>
2023-02-05 17:29:58 +0200

    task 4b

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -12,6 +12,7 @@ Description:	Answers to ex3
 void sumOfNums();
 void sumOfEvens();
 void operRandom();
+void collatzSeq();
 
 int main() 
 {
@@ -23,7 +24,8 @@ int main()
 	printf("\n===\n");
 	operRandom();
 	printf("\n===\n");
-	
+	collatzSeq();
+	printf("\n===\n");
 	return 0;
 }
 
@@ -84,6 +86,51 @@ void operRandom()
 	printf("Integer upper limit reached: %d", INT_MAX);
 }
 
+/*	Task 4 b)*/
+void collatzSeq() 
+{
+	time_t t = 0;
+	
+	srand((unsigned) time(&t));
+	
+	int randNum = rand() % 100;
+	
+	if (randNum == 0)
+	{
+		printf("===\n0\n");
+		
+	} 
+	else
+	{
+		int collatz[300] = {0};
+		//collatz[0] = randNum;
+		collatz[0] = 54;
+		
+		for (int i = 0; collatz[i]>0; i++)
+		{
+			printf("\n%d", collatz[i]);
+			
+			if (collatz[i] == 1) 
+			{
+				break;
+				
+			}
+			else if (collatz[i] % 2 == 0)
+			{
+				collatz[i+1] = collatz[i] / 2;
+			}
+			else 
+			{
+				collatz[i+1] = 3 * collatz[i] + 1;
+				
+			}
+			
+		}
 
+	}
+	
+	
+
+}
 
 
