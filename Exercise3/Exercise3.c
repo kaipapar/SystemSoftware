71df5ed53e44e4910027dbe266d2bed8730df244
Kaipapar <kkorsu@gmail.com>
2023-02-05 12:36:35 +0200

    Task 3 start

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -7,20 +7,47 @@ Description:	Answers to ex3
 #include <stdio.h>
 
 void sumOfNums();
+void sumOfEvens();
+void operRandom();
 
 int main() 
 {
-	int num1
-
+	
+	
+	sumOfNums(10);
+	sumOfEvens(10);
+	operRandom();
 	
 	return 0;
 }
 
-
-void sumOfNums() 
+/*	Task 3 a)	*/
+void sumOfNums(int n) 
 {
-	
-
-
-
+	int num = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		num += i;
+		printf("%d\n",num);
+	}
+	printf("%d\n",num);
 }
+
+/*	Task 3 b)	*/
+void sumOfEvens(int n) 
+{
+	int num = 0;
+	for (int i = 0; i <= n; i++) 
+	{
+		if (i % 2 == 0) 
+		{
+			num += i;
+			printf("%d\n",num);
+		}
+		
+	}
+	printf("%d\n",num);
+}
+
+/*	Task 3 c)*/
+
