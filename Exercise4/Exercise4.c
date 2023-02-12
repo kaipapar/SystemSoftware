64b0b10c184467e119b4f8c4eae05e6f6d674f1c
Kaipapar <kkorsu@gmail.com>
2023-02-12 13:07:19 +0200

    Ex4 3,ab

--- Exercise4/Exercise4.c
+++ Exercise4/Exercise4.c
@@ -1,18 +1,25 @@
 /*
 File:			Exercise4.c	
 Author:			Karri Korsu
-Description:	Answers to tasks 3 - 5
+Description:	Answers to Tasks 3 - 5
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define ARRLEN 100
 
+/*	Task 3	*/
 int* createIntArray();
 void numOfNeg(int array[]);
 void sumOfInt(int array[]);
+void sumOfPosDivThree(int array[]);
 
+/*	Task 4	*/
+int randNumGen(int num1, int num2);
+
+/*	Misc	*/
 void printArray(int array[]);
 void printArrayTilZero(int array[]);
 
@@ -24,13 +31,15 @@ int main()
 
     numOfNeg(intArray);
 	sumOfInt(intArray);
+	sumOfPosDivThree(intArray);
 
     //printArray(intArray);
     
     return 0;
 }
 
-/*	Creates an array consisting of integers which the user inputs	*/
+
+/*	3 Creates an array consisting of integers which the user inputs	*/
 int* createIntArray()
 {
     int *array = (int*)malloc(ARRLEN * sizeof(int));
@@ -54,41 +63,79 @@ int* createIntArray()
     return array;
 }
 
-/*	Returns the amount of negative numbers in an array	*/
+
+/*	3a Returns the amount of negative numbers in an array	*/
 void numOfNeg(int array[])
 {
     int numberOfNegs = 0;
-    for (int i = 0; i < ARRLEN; i++)
-    {
-        if (array[i] < 0)
-        {
-            numberOfNegs++;
-        }
-        else if (array[i] == 0)
-        {
-            break;
-        }
-    }
+	int i = 0;
+
+	while (array[i] != 0)
+	{
+		if (array[i] < 0)
+		{
+			numberOfNegs++;
+		}
+		i++;
+	}
     printf("number of negs: %d\n", numberOfNegs);
 }
 
-/*	Returns the sum of all integers in an array	*/
+
+/*	3b Returns the sum of all integers in an array	*/
 void sumOfInt(int array[])
 {
 	int sumOfIntegers = 0;
-	for (int i = 0; i < ARRLEN; i++)
+	int i = 0;
+	while (array[i] != 0)
 	{
-		if (array[i] == 0)
-		{
-			break;
-		}
-		else
-		{
-			sumOfIntegers += array[i];
-		}
+		sumOfIntegers += array[i];
+		i++;
 	}
 	printf("The sum of integers in the array: %d\n", sumOfIntegers);
 }
+
+
+/*	3c Returns the sum of positive integers in an array 
+	which can be divided by three	*/
+void sumOfPosDivThree(int array[])
+{
+	int sumOfPositives = 0;
+	int i = 0;
+	while (array[i] != 0)
+	{
+		if (array[i] > 0 && (array[i] % 3) == 0)
+		{
+		sumOfPositives += array[i];
+		}
+		i++;
+	}
+	printf("Sum of positive integers which can be divided by three: %d",
+	 sumOfPositives);
+}
+
+/*	4a Returns a random integer in range which is taken as an argument*/
+/*	Random number generator		*/
+int randNumGen(int num1, int num2)
+{
+	time_t t = 0;
+	srand((unsigned) time(&t));
+	int range = 0;
+	
+	if (num1 > num2)
+	{
+		range = (num1 - num2 + 1) + num2;
+	}
+	else if (num2 < num1)
+	{
+		range = (num2 - num1 + 1) + num1;
+	}
+	
+	int randNum = rand() % range;
+	
+	return randNum;
+}
+
 
 
 /*	Prints all elements of an integer array	*/
@@ -99,6 +146,7 @@ void printArray(int array[])
         printf("array element %d: %d\n", i, array[i]);
     }
 }
+
 
 /*	Prints all elements of an integer array until the next element is 0*/
 void printArrayTilZero(int array[])
