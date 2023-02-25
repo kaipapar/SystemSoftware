7915d569c5f01962c3c1db4bde7f43aa7331f504
Kaipapar <kkorsu@gmail.com>
2023-02-25 09:52:22 +0200

    Ex5 start task 3

--- Exercise5/Exercise5.c
+++ Exercise5/Exercise5.c
@@ -3,3 +3,46 @@ File:			Exercise5.c
 Author:			Karri Korsu
 Description:	Answers to tasks 3 and 4
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10000000
+#define MIN 0
+
+
+int main (int argc, char *argv[]) 
+{
+	int i = 0;
+
+
+	printf("Arguments: %d\n", argc);
+	printf("Arg 1 : %s\n", argv[1]);
+	
+	if (argv[1][0] == "-")
+	{
+		i = 1;
+		
+	}
+
+	for (i; )
+	{
+	
+	}
+
+	int argv1 = atoi(argv[1]);
+	
+	if (argc == 2 && argv1 < MAX && MIN < argv1)
+	{
+		
+		printf("%s", argv[1]);
+	
+	} else
+	{
+		printf("Your arguments are too many, too few or not between 10000000 and 0.\n");
+		printf("Argument count: %d\n", argc - 1);
+	}
+	
+	
+	return 0;
+}
