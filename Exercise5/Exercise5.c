e41592e086895a6f415fdd2288a52235ae2548bd
Kaipapar <kkorsu@gmail.com>
2023-02-25 18:06:11 +0200

    task 3a done

--- Exercise5/Exercise5.c
+++ Exercise5/Exercise5.c
@@ -1,4 +1,4 @@
-/*
+	/*
 File:			Exercise5.c
 Author:			Karri Korsu
 Description:	Answers to tasks 3 and 4
@@ -6,6 +6,7 @@ Description:	Answers to tasks 3 and 4
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 10000000
 #define MIN 0
@@ -14,21 +15,54 @@ Description:	Answers to tasks 3 and 4
 int main (int argc, char *argv[]) 
 {
 	int i = 0;
-
-
+	int flag = 0;
+	int arglen = strlen(argv[1]);
+	
 	printf("Arguments: %d\n", argc);
 	printf("Arg 1 : %s\n", argv[1]);
-	
-	if (argv[1][0] == "-")
-	{
-		i = 1;
-		
-	}
 
-	for (i; )
-	{
 	
+	
+	if (arglen <= 7)
+	{
+		if (argv[1][0] == 45 || (argv[1][0] >= 48 && argv[1][0] <= 57))
+		{	
+			for (i = 1; i < arglen; i++)
+			{
+				if (argv[1][i] >= 48 && argv[1][i] <= 57)
+				{	// if the element is a number: advance.
+					flag = 1;
+				}
+				else 
+				{					
+					flag = 0;
+					break;
+				}
+			}
+		}
 	}
+	
+	else 
+	{
+		flag = 0;
+	}
+	
+	if (flag == 1)
+	{
+		printf("This is your argument: %s \n", argv[1]);
+	}
+	else
+	{
+		printf("Argument is not valid.\n");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 	int argv1 = atoi(argv[1]);
 	
