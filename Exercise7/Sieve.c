33cbaa3061683ca46ffec243d2d902b948311d6a
Kaipapar <kkorsu@gmail.com>
2023-03-29 14:00:59 +0300

    Ex7 starting

--- Exercise7/Sieve.c
+++ Exercise7/Sieve.c
@@ -1,7 +1,7 @@
 /* 
- * File:   		Sieve.c
- * Author:		
- * Description: 
+ * File:   			Sieve.c
+ * Author:			Karri Korsu
+ * Description: 	An implementation of the sieve of erasthotenes for sys software ex7
  */
 
 #include <stdio.h>
@@ -10,6 +10,16 @@
 void testSieve() {
 
 	printf("Hello from Sieve.\n");
+	
+	
+	// ask user for an input to define an upper limit for the primes
+		//use user input function from previous exercise - returns a char array --> needs to be error checked and changed into an integer (n)
+	
+	// create an array sizeof n, with malloc, fill the array with numbers from 2-n, use pointer arithmetics
+	
+	// mark all numbers that are divisible by the iterator while iterator² is less or equal to n by changing their value to 0, when looping through the array 
+	
+	// return array elements that are not 0
 
 
 
